a2a16d0793240581f29a4312aa10f42b24f6e59b
yakup-kalkan <yakup.kalkan@hotmail.de>
2024-12-17 11:09:04 +0100

    #NEW: Card, CardContainer, Header, Footer were added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Header from "./components/Header";
+import CardContainer from "./components/CardContainer";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
     <>
       <div>
-        <p>Hello</p>
+        <Header/>
+        <CardContainer/>
+        <Footer/>
       </div>
     </>
   );
---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const data = [
+    {
+      id: "1",
+      title: "Today-Good1",
+      img: "http://placekitten.com/200/300", // Example image placeholder
+      content: "sdfksdfslkd",
+      date: "2024-11-16T15:28:37.332Z",
+    },
+    {
+      id: "2",
+      title: "Today-Good2",
+      img: "http://placekitten.com/200/300", // Example image placeholder
+      content: "sdfksdfslkd",
+      date: "2024-12-16T15:28:37.332Z",
+    },
+    {
+      id: "3",
+      title: "Today-Good3",
+      img: "http://placekitten.com/200/300", // Example image placeholder
+      content: "sdfksdfslkd",
+      date: "2024-13-16T15:28:37.332Z",
+    },
+    {
+      id: "4",
+      title: "Today-Good4",
+      img: "http://placekitten.com/200/300", // Example image placeholder
+      content: "sdfksdfslkd",
+      date: "2024-14-16T15:28:37.332Z",
+    },
+    {
+      id: "5",
+      title: "Today-Good5",
+      img: "http://placekitten.com/200/300", // Example image placeholder
+      content: "sdfksdfslkd",
+      date: "2024-15-16T15:28:37.332Z",
+    },
+    {
+      id: "6",
+      title: "Today-Good6",
+      img: "http://placekitten.com/200/300", // Example image placeholder
+      content: "sdfksdfslkd",
+      date: "2024-16-16T15:28:37.332Z",
+    },
+  ];
+
+
+export default function CardContainer() {
+  return (
+    <div>
+      <p>CardContainer</p>
+    </div>
+  )
+}
+
+
