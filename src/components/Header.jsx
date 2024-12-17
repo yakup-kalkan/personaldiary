5620fc0f6ccd40c3e57dc07f81b33f39cd092a39
yakup-kalkan <yakup.kalkan@hotmail.de>
2024-12-17 11:54:00 +0100

    #NEW: new logo and 'Add entry' button were  added to the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-      <p>Header</p>
-    </div>
+    <header className='bg-white shadow-md p-4 flex items-center justify-between'>
+        {/* Logo */}
+        <img src="./src/img/logo.jpg" className='h-24'/>
+        {/* Add Entry Button */}
+        <button className='bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition'>
+            Add Entry
+        </button>
+    </header>
+
   )
-}
+};
