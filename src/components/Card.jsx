f79e5f0d9468b5fd83533d5e1641a4bcf5d4adbf
yakup-kalkan <yakup.kalkan@hotmail.de>
2025-01-01 14:28:49 +0100

    the height of cards were changed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,10 @@ const Card = ({ id, title, date, img, imgUrl, content, onClick }) => {
   return (
     <div
       onClick={() => onClick({ id, title, date, img, imgUrl, content })}
-      className=" min-h-screen bg-white shadow-md rounded-lg overflow-hidden transform transition duration-300 hover:scale-105 hover:shadow-lg"
+      className="w-80 h-96 bg-white shadow-md rounded-lg overflow-hidden transform transition duration-300 hover:scale-105 hover:shadow-lg"
     >
-      <img src={img} alt={title} className=' h-48 object-cover' />
-      <div className="pl-4 pr-1 flex justify-between">
+      <img src={img} alt={title} className="h-48 w-full object-cover" />
+      <div className="p-4">
         <h2 className="text-lg font-bold text-gray-800">{title}</h2>
         <p className="text-sm text-gray-500">{new Date(date).toLocaleDateString()}</p>
       </div>
